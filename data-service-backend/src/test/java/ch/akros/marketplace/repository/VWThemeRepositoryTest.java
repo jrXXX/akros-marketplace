f3aff3000fc7485998a38304bc064f5576a22918
Richter Jan <richja@akros.ch>
2022-01-13 11:06:27 +0100

    junit integration test fixed

--- data-service-backend/src/test/java/ch/akros/marketplace/repository/VWThemeRepositoryTest.java
+++ data-service-backend/src/test/java/ch/akros/marketplace/repository/VWThemeRepositoryTest.java
@@ -10,13 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 
 import ch.akros.marketplace.AkrosMarketplacedataServiceApplication;
 import ch.akros.marketplace.entity.VWTheme;
 
 @SpringBootTest
 @ContextConfiguration(classes = AkrosMarketplacedataServiceApplication.class)
-@TestPropertySource(locations = "classpath:application-test.yml")
+@TestPropertySource("classpath:postgres_db_url.properties")
+@Transactional
 public class VWThemeRepositoryTest {
 	@Autowired
 	private VWThemeRepository vwThemeRepository;
