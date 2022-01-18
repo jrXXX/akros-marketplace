876196ed5c4f7f4d958b27d4cdf0d9edbb28ad81
Talla Berthold <tallbe@akros.ch>
2022-01-18 10:59:50 +0100

    added https to run dockerservice

--- data-service-backend/src/main/java/ch/akros/marketplace/AkrosMarketplacedataServiceApplication.java
+++ data-service-backend/src/main/java/ch/akros/marketplace/AkrosMarketplacedataServiceApplication.java
@@ -2,13 +2,21 @@ package ch.akros.marketplace;
 
 import java.util.Properties;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AkrosMarketplacedataServiceApplication {
 	private static final String POSTGRES_DB_HOST_ENV = "POSTGRES_DB_HOST";
-
+	 //HTTP port
+    @Value("${http.port}")
+    private int httpPort;
+    
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(AkrosMarketplacedataServiceApplication.class);
 
@@ -27,5 +35,18 @@ public class AkrosMarketplacedataServiceApplication {
 
 		application.run(args);
 	}
+	
+	 @Bean
+	    public ServletWebServerFactory servletContainer() {
+	        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+	        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+	        return tomcat;
+	    }
+
+	    private Connector createStandardConnector() {
+	        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+	        connector.setPort(httpPort);
+	        return connector;
+	    }
 
 }
